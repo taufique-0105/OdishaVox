8b663e166b409ddf2293537c4cab32ad62d8afbd
Taufique Alam Ansari <taufiquealam0105@gmail.com>
2025-05-30 18:50:35 +0530

    ..

--- 01version/frontend/components/TTSPlayer.js
+++ 01version/frontend/components/TTSPlayer.js
@@ -20,7 +20,7 @@ const TTSComponent = ({ defaultLanguage = "en-IN", initialText = "" }) => {
 
   const player = useAudioPlayer(audioUri);
 
-  const URI = "http://<YOUR_IP_ADDRESS>:3000/api/v1/tts";
+  const URI = "http://<YOUR-IP-ADDRESS>:3000/api/v1/tts";
 
   const fetchTTS = async () => {
     if (!text.trim()) {
